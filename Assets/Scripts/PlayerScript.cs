19cea02a935dd79ff729fda105867c18ddaf0087
moumojan <you@example.com>
2017-01-21 20:36:12 +0900

    3Dモデル以外ほぼ完成

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour {
     [SerializeField]
-    public int playerNum { get; private set; }
+    private int playerNum;
 
     [SerializeField]
     private float maxSpeed;
@@ -23,15 +24,23 @@ public class PlayerScript : MonoBehaviour {
     Rigidbody rigid;
     GameManagerScript manager;
 
+    [SerializeField]
+    GameObject arrow;
+
+    Slider slider;
+    bool slider_direction_right = true;
     // Use this for initialization
     void Start() {
         rigid = GetComponent<Rigidbody>();
         manager = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+        slider = GameObject.Find("Slider").GetComponent<Slider>();
 
-        active = false;
+        /*active = true;
+        //active = false;
         turnPhase = 0;
         powerTimer = 0;
-        getDirection();
+        getDirection();*/
+        SetPlayerActivate(playerNum == 0 ? true : false);
     }
 	
 	// Update is called once per frame
@@ -45,6 +54,7 @@ public class PlayerScript : MonoBehaviour {
                 {
                     powerTimer = 0;
                     turnPhase++;
+                    arrow.SetActive(false);
                 }
 
                 //Rotation with left/right button
@@ -53,18 +63,43 @@ public class PlayerScript : MonoBehaviour {
                     newDirection += directionOffset;
                     //TO BE ADDED
                     //Texture.transform.rotaion = transform.rotaion * Quaternion.Euler(0, newDirection, 0);
+                    transform.RotateAround(transform.position, transform.up, Time.deltaTime * -90f);
                 }
 
                 if (Input.GetKey(KeyCode.RightArrow))
                 {
                     newDirection -= directionOffset;
                     //TO BE ADDED
+                    transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
                 }
             }
 
             else if (turnPhase == 1) //Wait for button release
             {
-                powerTimer++;
+                if(slider_direction_right)
+                {
+                    slider.value += 1.0f / 60.0f;
+                    if(slider.value >= 1.0f)
+                    {
+                        slider_direction_right = false;
+                        slider.value = 1.0f;
+                    }
+                }
+                else
+                {
+                    slider.value -= 1.0f / 60.0f;
+                    if (slider.value <= 0.0f)
+                    {
+                        slider_direction_right = true;
+                        slider.value = 0.0f;
+                    }
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    turnPhase++;
+                }
+                /*powerTimer++;
                 if(Input.GetKeyUp(KeyCode.Space))
                 {
                     speed = maxSpeed * (Mathf.Sin( (Mathf.Deg2Rad*powerTimer) % Mathf.PI) ); //movement speed
@@ -73,7 +108,7 @@ public class PlayerScript : MonoBehaviour {
                         turnPhase++;
                     else
                         turnPhase--;
-                }
+                }*/
             }
             else if(turnPhase == 2) //Move
             {
@@ -85,9 +120,13 @@ public class PlayerScript : MonoBehaviour {
                     speed * Mathf.Sin(Mathf.Deg2Rad * newDirection),
                     player.transform.position.z);
              */
+                Vector3 forward = transform.forward;
+                float power = slider.value;
 
-                newDirection = newDirection * Mathf.Deg2Rad;
-                rigid.AddForce(speed * new Vector3(Mathf.Cos(newDirection), 0, Mathf.Sin(newDirection))) ;
+                //newDirection = newDirection * Mathf.Deg2Rad;
+                //rigid.AddForce(speed * new Vector3(Mathf.Cos(newDirection), 0, Mathf.Sin(newDirection))) ;
+                rigid.AddForce(forward * power * 1000.0f);
+                //rigid.AddForce(Vector3.up * 100.0f);
                 manager.PlayerShoot();
 
                 turnPhase++;
@@ -97,7 +136,17 @@ public class PlayerScript : MonoBehaviour {
                 //togglePlayerActivate(false);
             }
         }
+
+        CheckGameEnd();
 	}
+
+    void CheckGameEnd()
+    {
+        if(transform.position.y <= -5.0f)
+        {
+            manager.GameEnd(playerNum);
+        }
+    }
 
     public virtual void togglePlayerActivate(bool activate)
     {
@@ -114,8 +163,33 @@ public class PlayerScript : MonoBehaviour {
         //transform.rotation.SetEulerAngles(new Vector3(0, 0, 0));
     }
 
-    bool IsRigidbodyVelocity()
+    public bool IsRigidbodyVelocityZero()
     {
-        return true;
+        if(rigid.velocity.magnitude < 0.01f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void SetPlayerActivate(bool activate)
+    {
+        if (activate)
+        {
+            active = activate;
+            turnPhase = 0;
+            getDirection();
+            arrow.SetActive(true);
+        }
+        else
+        {
+            active = activate;
+            turnPhase = 0;
+            getDirection();
+            arrow.SetActive(false);
+        }
     }
 }
