6c9841642dbe2ca20afd3cb7e3ef6bd2d00551a0
moumojan <moumojan@gmail.com>
2017-01-22 14:07:51 +0900

    3Dモデル及びサウンド追加、地面の動き追加

---
+++ Assets/Scripts/PlayerOilDrop.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOilDrop : MonoBehaviour {
+    public bool oildrop_on { get; private set; }
+    [SerializeField]
+    GameObject oildropIcon;
+
+    bool move_start = false;
+    Rigidbody rigid;
+
+    Vector3 startPos;
+
+    [SerializeField]
+    float oil_span;
+
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(oildrop_on && !move_start && rigid.velocity.magnitude >= 0.01f)
+        {
+            move_start = true;
+
+            startPos = transform.position;
+        }
+
+        if(move_start)
+        {
+            DropOil();
+
+            if(rigid.velocity.magnitude < 0.01f)
+            {
+                move_start = false;
+                oildrop_on = false;
+
+                oildropIcon.SetActive(false);
+            }
+        }
+	}
+
+    public void OilDropOn()
+    {
+        oildrop_on = true;
+        oildropIcon.SetActive(true);
+    }
+
+    void DropOil()
+    {
+        if(Vector3.Distance(transform.position, startPos) >= oil_span)
+        {
+            //Debug.Log("oil");
+            GameObject obj = (GameObject)Instantiate(Resources.Load("Oil"), GameObject.Find("Oils").transform);
+            obj.GetComponent<OilScript>().Init(transform.position, GetComponent<PlayerScript>().playerNum);
+
+            startPos = transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPowerUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPowerUp : MonoBehaviour {
+    public bool powerup_on { get; private set; }
+    [SerializeField]
+    GameObject powerupIcon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PowerUpOn()
+    {
+        powerup_on = true;
+        powerupIcon.SetActive(true);
+    }
+
+    public void ResetPowerUpFlag()
+    {
+        powerup_on = false;
+        powerupIcon.SetActive(false);
+    }
+}
